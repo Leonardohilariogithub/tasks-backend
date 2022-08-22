c0f2716bb40e309626fb0204505a93ab9f0a4df6
Leonardo C. Hilário <hilarioleozinho@gmail.com>
2022-08-22 20:38:52 -0300

    teste unitario

---
+++ src/test/java/br/ce/wcaquino/taskbackend/utils/DateUtilsTest.java
@@ -0,0 +1,17 @@
+package br.ce.wcaquino.taskbackend.utils;
+
+import static org.junit.Assert.*;
+
+import java.time.LocalDate;
+
+import org.junit.Test;
+
+public class DateUtilsTest {
+
+	@Test
+	public void test() {
+		LocalDate date = LocalDate.of(2030, 01, 01);
+		DateUtils.isEqualOrFutureDate(date);
+	}
+
+}
